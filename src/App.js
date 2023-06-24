30fb8506d3e3bbf6f25d784853c3a1663d2e34eb
AndrewDeras <andrewderas7@gmail.com>
2023-06-24 20:45:32 -0300

    some fixes in card component and global css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react';
 import Card from './components/card/index';
-import Nav from './components/nav';
-import axios from 'axios';
 const url = 'https://games-test-api-81e9fb0d564a.herokuapp.com/api/data/';
 
 
@@ -10,19 +8,47 @@ function App() {
 
 
   useEffect(() => {
-
-    axios.get(url, {
-      headers: {
-        "dev-email-address": "andrew@gmail.com"
+    async function getData() {
+      try {
+        const response = await fetch(url, { headers: { "dev-email-address": "andrew@gmail.com" } });
+        const data = await response.json();
+        console.log(data);
+        localStorage.setItem('games', JSON.stringify(data));
+        setGames(data);
+      } catch (error) {
+        console.log(error);
       }
-    }).then(respose => setGames(respose.data)).catch(err => console.log(err));
-  }, [])
+    }
+
+    if (!localStorage.getItem('games')) {
+      getData();
+    } else {
+      setGames(JSON.parse(localStorage.getItem('games')));
+    }
+  }, []);
+
+
   return (
     <div className="App">
-      <Nav />
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a href='/' className="navbar-brand">Games Free To Play</a>
+          <select class="form-select" aria-label="Default select example">
+            <option selected>Filter by genre</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search by tile" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+
       <div className="row">
         {games &&
-          games.map((game, index) => < Card key={String(index)} game={game} index={index} />
+          games.map((game, index) => < Card key={String(index)} game={game} />
           )}
       </div>
     </div>
